91137e01cb0a915b631c593147f6eaae13af6f42
stevenchen95 <58769267+stevenchen95@users.noreply.github.com>
2022-03-29 17:34:36 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 export default {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '37398b28f79c4121b0fde225ff1fe81c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '陈十一的知识小站',
+  domain: 'https://handy-rhinoceros-20c.notion.site/37398b28f79c4121b0fde225ff1fe81c',
+  author: '陈十一',
 
   // open graph metadata (optional)
   description: 'Example site description',
